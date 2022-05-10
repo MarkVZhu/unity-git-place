ee3505944f3d44f8fe1347a5c3e81b06de8e61be
zianj <zianj@kean.edu>
2022-05-11 02:08:29 +0800

    Add the Mute Button

--- Assets/Creator-Kit/Scripts/MainMenu/MenuController.cs
+++ Assets/Creator-Kit/Scripts/MainMenu/MenuController.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
     [Header("Levels To load")]
     public string _newGameLevel;
 
@@ -14,6 +15,27 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject mainMenu = GameObject.Find("Main Menu Container");
+        GameObject muteButton = mainMenu.transform.Find("Mute Button").gameObject;
+        GameObject unmuteButton = mainMenu.transform.Find("Unmute Button").gameObject;
+        if(AudioListener.volume == 0) {
+            muteButton.SetActive(false);
+            unmuteButton.SetActive(true);
+        }
+    }
+
+    public void muteVolume(bool mute) {
+        if (mute) {
+            AudioListener.volume = 0;
+        } else {
+            AudioListener.volume = 1;
+        }
     }
  
 }
